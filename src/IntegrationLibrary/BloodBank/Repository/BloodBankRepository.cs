5c1bcd03cd9785cdc0f53f4a34f00cf450ca3681
Dejan Gloginjic <deki555@hotmail.com>
2022-10-25 21:56:51 +0200

    Implemented BloodBank model

---
+++ src/IntegrationLibrary/BloodBank/Repository/BloodBankRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.BloodBank.Repository
+{
+    public class BloodBankRepository : IBloodBankRepository
+    {
+        public void Create(BloodBank bloodBank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(BloodBank bloodBank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BloodBank> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BloodBank GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(BloodBank bloodBank)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
