bd925608e3ed511af1a4fc23dd868470cffe78d9
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-11-02 21:47:19 +0100

    merging

--- src/IntegrationAPI/Controllers/BloodBankController.cs
+++ src/IntegrationAPI/Controllers/BloodBankController.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using IntegrationAPI.Dtos.Response;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace IntegrationAPI.Controllers
