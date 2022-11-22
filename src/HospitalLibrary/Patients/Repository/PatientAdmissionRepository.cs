c6a92357915ee73d5f68f566c4f37ae1f62fc020
Ivana Lazarevic <93544830+lazarevicivana@users.noreply.github.com>
2022-11-22 21:47:36 +0100

    generate discharge report started

--- src/HospitalLibrary/Patients/Repository/PatientAdmissionRepository.cs
+++ src/HospitalLibrary/Patients/Repository/PatientAdmissionRepository.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using HospitalLibrary.Common;
 using HospitalLibrary.Patients.Model;
 using HospitalLibrary.Settings;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalLibrary.Patients.Repository
 {
@@ -8,6 +12,12 @@ namespace HospitalLibrary.Patients.Repository
     {
         public PatientAdmissionRepository(HospitalDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public Task<PatientAdmission> GetPatientAdmissionByIdAsync(Guid id)
+        {
+            return  Task.FromResult(DbSet.Include(x => x.Patient)
+                .FirstOrDefault(x => x.Id == id));
         }
     }
 }
--- src/HospitalLibrary/Patients/Service/PatientAdmissionService.cs
+++ src/HospitalLibrary/Patients/Service/PatientAdmissionService.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 using System.Threading.Tasks;
 using HospitalLibrary.Common;
 using HospitalLibrary.CustomException;
 using HospitalLibrary.Patients.Model;
 using HospitalLibrary.Rooms.Model;
 using HospitalLibrary.TreatmentReports.Model;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Drawing.Layout;
+using PdfSharpCore.Pdf;
 
 namespace HospitalLibrary.Patients.Service
 {
@@ -82,6 +87,7 @@ namespace HospitalLibrary.Patients.Service
             if (admission.DateOfDischarge != null) throw new PatientDischargeException("Patient is already discharged!");
             admission.Update(admissionRequest.ReasonOfDischarge,DateTime.Now);
             await UpdateRoomAvailability(admission);
+            GeneratePdfReport(admission);
             await _unitOfWork.PatientAdmissionRepository.UpdateAsync(admission);
             await _unitOfWork.CompleteAsync();
             return true;
@@ -94,6 +100,38 @@ namespace HospitalLibrary.Patients.Service
             updateBed.Update(true);
             await _unitOfWork.RoomBedRepository.UpdateAsync(updateBed);
             await _unitOfWork.CompleteAsync();
+        }
+
+        private void GeneratePdfReport(PatientAdmission admission)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            var document = new PdfDocument();
+            var page = document.AddPage();
+            var gfx = XGraphics.FromPdfPage(page);
+            var formatter = new XTextFormatter(gfx);
+            var layoutRectangle = new XRect(10, 10, page.Width, page.Height);
+            var font = new XFont("Verdana", 20, XFontStyle.Bold);
+            gfx.DrawString("Patient admission report", font, XBrushes.Black,
+                new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);  
+            var secondPage = document.AddPage();
+            var gfxBody = XGraphics.FromPdfPage(secondPage);
+            var formatterBody = new XTextFormatter(gfxBody);
+            var layoutRectangleBody = new XRect(10, 10, page.Width, page.Height);
+            var fontBody = new XFont("Verdana", 14, XFontStyle.Regular);
+            formatterBody.DrawString(GenerateTextInPdf(admission), fontBody, XBrushes.Black,layoutRectangleBody);
+            var filename = "PDFDocument.pdf";
+            document.Save(filename);
+            Process.Start(new ProcessStartInfo(filename) { UseShellExecute = true });
+        }
+
+        private String GenerateTextInPdf(PatientAdmission patientAdmission)
+        {
+            var admission = _unitOfWork.PatientAdmissionRepository.GetPatientAdmissionByIdAsync(patientAdmission.Id);
+            var sb = new StringBuilder();
+            sb.AppendLine("Patient : " + admission.Result.Patient.Name + " " + admission.Result.Patient.Surname).AppendLine(Environment.NewLine);
+            sb.AppendLine("Reason of hospitalizing : " + admission.Result.Reason).AppendLine(Environment.NewLine);
+            sb.AppendLine("Reason of discharge : " + admission.Result.ReasonOfDischarge).AppendLine(Environment.NewLine);
+            return sb.ToString();
         }
     }
 }
