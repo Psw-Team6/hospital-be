074fcbeb5c4e579123c14b5632ca87082a3f1ada
Kumici13 <lukapikula13@gmail.com>
2022-12-08 15:36:31 +0100

    Initial tests and logic

--- src/HospitalLibrary/Rooms/Service/RoomService.cs
+++ src/HospitalLibrary/Rooms/Service/RoomService.cs
@@ -36,5 +36,76 @@ namespace HospitalLibrary.Rooms.Service
             await _unitOfWork.CompleteAsync();
             return true;
         }
+
+        public async Task<Room> MergeRooms(Room room1, Room room2)
+        {
+            Room newRoom = new Room();
+            newRoom = room1;
+            //TREBACE OVO VRV foreach (var room2Bed in room2.Beds) newRoom.Beds.Add(room2Bed);
+            newRoom.Id = Guid.NewGuid();
+            GRoom newGroom = new GRoom();
+            
+            GRoom groom1 = await _unitOfWork.GRoomRepository.GetByIdAsync(room1.GRoomId);
+            GRoom groom2 = await _unitOfWork.GRoomRepository.GetByIdAsync(room2.GRoomId);
+            
+            newGroom.RoomId = newRoom.Id;
+            newGroom.Id = Guid.NewGuid();
+            newGroom.PositionX = Math.Min(groom1.PositionX, groom2.PositionX);
+            newGroom.PositionY = Math.Min(groom1.PositionY, groom2.PositionY);
+            newGroom.Lenght = Math.Max(groom1.PositionX + groom1.Lenght, groom2.PositionX + groom2.Lenght) - newGroom.PositionX;
+            newGroom.Width = Math.Max(groom1.PositionY + groom1.Width, groom2.PositionY + groom2.Width) - newGroom.PositionY;
+
+            await _unitOfWork.GRoomRepository.CreateAsync(newGroom);
+            await _unitOfWork.RoomRepository.CreateAsync(newRoom);
+            await _unitOfWork.GRoomRepository.DeleteAsync(groom1);
+            await _unitOfWork.GRoomRepository.DeleteAsync(groom2);
+            await _unitOfWork.RoomRepository.DeleteAsync(room1);
+            await _unitOfWork.RoomRepository.DeleteAsync(room2);
+            await _unitOfWork.CompleteAsync();
+
+            return newRoom;
+        }
+        
+        
+        public async Task<Room> SplitRoom(Room room1)
+        {
+            Room newRoom1 = new Room();
+            newRoom1 = room1;
+            newRoom1.Id = Guid.NewGuid();
+            
+            Room newRoom2 = new Room();
+            newRoom2.Id = Guid.NewGuid();
+            
+            GRoom originalGroom = await _unitOfWork.GRoomRepository.GetByIdAsync(room1.GRoomId);
+            
+            GRoom newGroom1 = new GRoom();
+            newGroom1.RoomId = newRoom1.Id;
+            newGroom1.Id = Guid.NewGuid();
+            newGroom1.PositionX = originalGroom.PositionX;
+            newGroom1.PositionY = originalGroom.PositionY;
+            newGroom1.Lenght = originalGroom.Lenght / 2;
+            newGroom1.Width = originalGroom.Width / 2;
+            
+            GRoom newGroom2 = new GRoom();
+            newGroom2.RoomId = newRoom2.Id;
+            newGroom2.Id = Guid.NewGuid();
+            newGroom2.Lenght = originalGroom.Lenght - newGroom1.Lenght;
+            newGroom2.Width = originalGroom.Width - newGroom1.Width;
+            newGroom2.PositionX = newGroom1.PositionX + newGroom1.Lenght;
+            newGroom2.PositionY = newGroom1.PositionY + newGroom1.Width;
+
+            await _unitOfWork.GRoomRepository.CreateAsync(newGroom1);
+            await _unitOfWork.GRoomRepository.CreateAsync(newGroom2);
+            
+            await _unitOfWork.RoomRepository.CreateAsync(newRoom1);
+            await _unitOfWork.RoomRepository.CreateAsync(newRoom2);
+            
+            await _unitOfWork.RoomRepository.DeleteAsync(room1);
+            await _unitOfWork.GRoomRepository.DeleteAsync(originalGroom);
+            
+            await _unitOfWork.CompleteAsync();
+
+            return newRoom1;
+        }
     }
 }
