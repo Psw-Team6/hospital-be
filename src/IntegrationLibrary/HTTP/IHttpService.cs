e69774ef4ebbdac9edda11032ffaceec8e3142cc
Tamara Antic <tasaantic00@gmail.com>
2022-12-11 20:42:46 +0100

    implement send requests

---
+++ src/IntegrationLibrary/HTTP/IHttpService.cs
@@ -0,0 +1,14 @@
+﻿using IntegrationLibrary.BloodRequests.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.HTTP
+{
+    public interface IHttpService
+    {
+        Task<BloodSupplyResponse> GetProductAsync(string path);
+    }
+}
