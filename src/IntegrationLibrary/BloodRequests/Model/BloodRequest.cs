143f4a4f18a04caf9c715fe6e61996b610b447c1
Tamara Antic <tasaantic00@gmail.com>
2022-12-16 07:25:23 +0100

    dejanove promjene

--- src/IntegrationLibrary/BloodRequests/Model/BloodRequest.cs
+++ src/IntegrationLibrary/BloodRequests/Model/BloodRequest.cs
@@ -13,11 +13,39 @@ namespace IntegrationLibrary.BloodRequests.Model
         public double Amount { get; set; }
         public String Reason { get; set; }
         public DateTime Date { get; set; }
-        public String DoctorUsername { get; set; } //treba da se promijeni na ulogovanog doktora
+        public String DoctorUsername { get; set; } 
         public Status Status { get; set; }
         public String Comment { get; set; }
 
         public Guid BloodBankId { get; set; }
 
+        
+        public bool isPending()
+        {
+            if (this.Status == Status.PENDING)
+                return true;
+            else
+                return false;
+        }
+
+        public bool isApproved()
+        {
+            if (this.Status == Status.APPPROVED)
+                return true;
+            else
+                return false;
+
+        }
+
+        public bool isRejected()
+        {
+            if (this.Status == Status.REJECTED)
+                return true;
+            else
+                return false;
+
+        }
+
     }
+
 }
