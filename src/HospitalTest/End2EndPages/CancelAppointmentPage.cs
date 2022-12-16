c9f60ec61ab6caeb29e28185fe0dea6f70ee5643
Nemanja2106 <nemkejelic15@gmail.com>
2022-12-16 03:42:20 +0100

    e2e test

---
+++ src/HospitalTest/End2EndPages/CancelAppointmentPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HospitalTest.End2EndCommon;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HospitalTest.End2EndPages
+{
+    public class CancelAppointmentPage
+    {
+        private readonly IWebDriver _driver;
+        private const string Uri = "http://localhost:4200/my-appointments";
+        private IWebElement CancelButton => _driver.FindElement(By.Id("cancelButton"));
+        private IWebElement ChangePageButton => _driver.FindElement(By.Id("mat-tab-label-0-2"));
+        public CancelAppointmentPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Navigate() => _driver.Navigate().GoToUrl(Uri);
+
+        public void Cancel()
+        {
+            CancelButton.Click();
+        }
+        
+        public void WaitForFormSubmit()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 1));
+        }
+
+        public void ChangeDay()
+        {
+            ChangePageButton.Click();
+        }
+    }
+}
