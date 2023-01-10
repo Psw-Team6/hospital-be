c5be3857e89681beb4f3f1c01a5f1f164ff4bb6a
Kumici13 <lukapikula13@gmail.com>
2023-01-10 23:46:04 +0100

    services

--- src/HospitalAPI/Controllers/RoomEventController.cs
+++ src/HospitalAPI/Controllers/RoomEventController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using HospitalAPI.Dtos.Request;
@@ -30,6 +31,7 @@ namespace HospitalAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<RoomEventResponse>> CreateEvent([FromBody] RoomEventRequest roomEventDto)
         {
+            Console.WriteLine("POGODJEN ZA EVENJTE");
             var roomEvent = _mapper.Map<RoomEvent>(roomEventDto);
             var roomEventCreated = await _roomEventService.Create(roomEvent);
             
@@ -41,5 +43,95 @@ namespace HospitalAPI.Controllers
             return Ok(roomEventCreated);
         }
 
+        [HttpGet("/api/v1/Merging-succesful")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int>> GetSuccesfullMergingCount()
+        {
+            var mergingCount = await _roomEventService.SuccesfullMergingCount();
+            return Ok(mergingCount);
+        }
+
+        [HttpGet("/api/v1/Spliting-succesful")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int>> GetSuccesfullSplitingCount()
+        {
+            var splitingCount = await _roomEventService.SuccesfullSplitingCount();
+            return Ok(splitingCount);
+        }
+
+        [HttpGet("/api/v1/Merging-step-count")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int[]>> GetMergingStepCount()
+        {
+            var mergingStepCount = await _roomEventService.StepMergingCount();
+            return Ok(mergingStepCount);
+        }
+
+        [HttpGet("/api/v1/Spliting-step-count")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int[]>> GetSplitingStepCount()
+        {
+            var splitingStepCount = await _roomEventService.StepSplitingCount();
+            return Ok(splitingStepCount);
+        }
+        
+        
+        [HttpGet("/api/v1/Scheduling-cancel-count")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int>> GetSchedulingCancelCount()
+        {
+            var Count = await _roomEventService.SchedulingCanceledCount();
+            return Ok(Count);
+        }
+        
+        [HttpGet("/api/v1/GetEventsInLastDay")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<RoomEvent>>> GetEventsInLastDay()
+        {
+            var events = await _roomEventService.GetRoomEventsInLastDay();
+            return Ok(events);
+        }
+        
+        [HttpGet("/api/v1/Average-merging-scheduling-time")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int[]>> GetAverageMergingSchedulingTime()
+        {
+            var splitingStepCount = await _roomEventService.GetAverageMergningSchedulingTimes();
+            return Ok(splitingStepCount);
+        }
+        
+        [HttpGet("/api/v1/Average-merging-step-time")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int[]>> GetAverageMergingStepTime()
+        {
+            var splitingStepCount = await _roomEventService.GetAverageMergningStepTimes();
+            return Ok(splitingStepCount);
+        }
+        
+        [HttpGet("/api/v1/Average-spliting-scheduling-time")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int[]>> GetAverageSplitingSchedulingTime()
+        {
+            var splitingStepCount = await _roomEventService.GetAverageSplitingSchedulingTimes();
+            return Ok(splitingStepCount);
+        }
+        
+        [HttpGet("/api/v1/Average-spliting-step-time")]
+        [ProducesResponseType( StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<int[]>> GetAverageSplitingStepTime()
+        {
+            var splitingStepCount = await _roomEventService.GetAverageSplitingStepTimes();
+            return Ok(splitingStepCount);
+        }
     }
 }
--- src/HospitalLibrary/Rooms/Service/IRoomEventService.cs
+++ src/HospitalLibrary/Rooms/Service/IRoomEventService.cs
@@ -8,5 +8,16 @@ namespace HospitalLibrary.Rooms.Service
     {
         Task<IEnumerable<RoomEvent>> GetAll();
         Task<RoomEvent> Create(RoomEvent roomEvent);
+        Task<int> SuccesfullMergingCount();
+        Task<int> SuccesfullSplitingCount();
+        Task<int[]> StepMergingCount();
+        Task<int[]> StepSplitingCount();
+        Task<int> SchedulingCanceledCount();
+        
+        Task<IEnumerable<RoomEvent>> GetRoomEventsInLastDay();
+        Task<int[]> GetAverageMergningSchedulingTimes();
+        Task<int[]> GetAverageMergningStepTimes();
+        Task<int[]> GetAverageSplitingSchedulingTimes();
+        Task<int[]> GetAverageSplitingStepTimes();
     }
 }
