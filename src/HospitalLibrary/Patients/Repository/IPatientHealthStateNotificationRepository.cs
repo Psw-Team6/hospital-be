3ecf0a4d60e5abdb1f6ddf98f0adf8ac01144bf7
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2023-01-12 21:05:13 +0100

    fet: patient state test

--- src/HospitalLibrary/Patients/Repository/IPatientHealthStateNotificationRepository.cs
+++ src/HospitalLibrary/Patients/Repository/IPatientHealthStateNotificationRepository.cs
@@ -1,10 +1,13 @@
-﻿using HospitalLibrary.Common;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HospitalLibrary.Common;
 using HospitalLibrary.Patients.Model;
 
 namespace HospitalLibrary.Patients.Repository
 {
     public interface IPatientHealthStateNotificationRepository:IGenericRepository<PatientHealthStateNotification>
     {
-        
+        public Task<List<PatientHealthStateNotification>> GetAllNotifications(Guid patientId);
     }
 }
