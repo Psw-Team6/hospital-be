fe0e900dc5f3c4c0c42f47bd6f2dcbe9d91f6fd7
Ivana Lazarevic <93544830+lazarevicivana@users.noreply.github.com>
2023-01-12 14:49:33 +0100

    examination analysing function implemented

---
+++ src/HospitalAPI/Controllers/Private/EventStoreExaminationController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using HospitalLibrary.Examinations.EventStores;
+using HospitalLibrary.Examinations.Service.EventStoreService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAPI.Controllers.Private
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    /*
+    [HospitalAuthorization(UserRole.Manager)]
+    */
+    public class EventStoreExaminationController:ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly EventStoreExaminationService _eventStoreExaminationService;
+
+        public EventStoreExaminationController(IMapper mapper, EventStoreExaminationService eventStoreExaminationService)
+        {
+            _mapper = mapper;
+            _eventStoreExaminationService = eventStoreExaminationService;
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<int>> GetAverageStepCount()
+        {
+            var result = await _eventStoreExaminationService.GetAverageStepCount();
+            return Ok(result);
+        } 
+        [HttpGet("get-time")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<TimeSpan>> GetAverageTime()
+        {
+            var result = await _eventStoreExaminationService.GetAverageTime();
+            return Ok(result);
+        }
+
+        [HttpGet("get-average-count-type")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Dictionary<EventStoreExaminationType, int>>> GetAverageCountAllTypes()
+        {
+            var result = await _eventStoreExaminationService.GetAverageCountForEveryStep();
+            return Ok(result);
+        }
+        [HttpGet("get-average-time-type")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Dictionary<EventStoreExaminationType, int>>> GetAverageTimeForEveryStep()
+        {
+            var result = await _eventStoreExaminationService.GetAverageTimeForEveryStep();
+            return Ok(result);
+        }
+    }
+}
