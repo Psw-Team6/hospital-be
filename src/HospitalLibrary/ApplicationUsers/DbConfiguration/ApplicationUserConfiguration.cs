e38bd3190c54482793752b2c5c9f64d0d7f4a690
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2023-01-16 18:29:48 +0100

    aplication user pgone and jmbg update

--- src/HospitalLibrary/ApplicationUsers/DbConfiguration/ApplicationUserConfiguration.cs
+++ src/HospitalLibrary/ApplicationUsers/DbConfiguration/ApplicationUserConfiguration.cs
@@ -16,11 +16,13 @@ namespace HospitalLibrary.ApplicationUsers.DbConfiguration
                 .IsUnique();// _ = builder.HasKey(x => x.Id);
             _ = builder.Property(x => x.Email)
                 .IsRequired();
-            _ = builder.Property(x => x.Jmbg)
-                .HasColumnType("jsonb");
-            _ = builder.Property(x => x.Phone)
-                .HasColumnType("jsonb");
             
+            // _ = builder.Property(x => x.Jmbg)
+            //     .HasColumnType("jsonb");
+            // _ = builder.Property(x => x.Phone)
+            //     .HasColumnType("jsonb");
+            
+
         }
     }
 }
